9e08eea4c6c723565331d49f4b821ff5ad5c4c54
Aspen Smith <aspen.smith@epicpki.local>
2023-04-18 19:32:13 -0600

    Initialize Test project

---
+++ VmwareVcenterOrchestratorTest/Program.cs
@@ -0,0 +1,21 @@
+// See https://aka.ms/new-console-template for more information
+using Keyfactor.Extensions.Orchestrator.VmwareVcenterOrchestrator.Client;
+using Microsoft.Extensions.Logging;
+using Keyfactor.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Keyfactor.Orchestrators.Common.Enums;
+using Keyfactor.Orchestrators.Extensions;
+using Newtonsoft.Json;
+
+namespace Keyfactor.Extensions.Orchestrator.VmwareVcenterOrchestratorTest.Program
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            //var client = new VmwareVsphereClient();
+        }
+    }
+}
